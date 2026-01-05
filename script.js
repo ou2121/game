a146276b6a81df53868bb00fd6dea51dae18518b
oussama <ou2121>
2026-01-05 22:41:29 +0100

    add result ui and three buttons

--- script.js
+++ script.js
@@ -1,21 +1,79 @@
+
+
 function getComputerChoice(){
-let ch=Math.random();
-let str="";
-if(ch<=0.33){
-str="Rock";
-}
-else if(ch<=0.67){
-str="Papper";
-}else{
-    str="Scissors";
-}
-return str;
-}
-console.log(getComputerChoice())
+let number=Math.random();
+ let computerchoice="";
+if(number<=0.33){computerchoice="rock";}
+else if(number<=0.67){computerchoice="paper";}
+else{    computerchoice="scissors";}
+return computerchoice;}
+
 function getHumainChoice(){
-    let cho =  prompt("enter your choice:");
-    return cho;
-}
-console.log(getHumainChoice());
+    let humainchoice ;
+    return humainchoice;}
 let humainScore = 0;
 let computerScore = 0;
+
+
+function playRound(humainchoice,computerchoice){
+  
+    if(humainchoice === computerchoice){
+       
+      return "it's a tie";
+
+    }
+    if(((humainchoice === "paper" && computerchoice === "rock") ||
+    (humainchoice === "scissors" && computerchoice === "paper") ||
+    (humainchoice === "rock" && computerchoice === "scissors"))){
+        humainScore++;
+        return "you win this round!";
+        
+}else {
+    computerScore++;
+ return  "computer win this round";
+    
+}}
+
+
+const computerselection = getComputerChoice();
+
+
+const rock = document.getElementById("rock")
+const paper = document.getElementById("paper")
+const scissors = document.getElementById("scissors")
+
+ const rsl= document.getElementById("result");
+rock.addEventListener("click",function(event){
+    
+    
+const computerselection = getComputerChoice();
+    
+   
+    rsl.innerText=`${ playRound("rock",computerselection) }`
+console.log(computerselection)
+    
+    });
+paper.addEventListener("click",function(event){
+    
+  
+const computerselection = getComputerChoice();
+   
+   
+    rsl.innerText=`${   playRound("paper",computerselection) }`
+    console.log(computerselection)
+    
+    });
+scissors.addEventListener("click",function(event)
+{   
+   const computerselection = getComputerChoice();
+  rsl.innerText=`${ playRound("scissors",computerselection)}`
+  console.log(computerselection)
+ })
+
+   
+   
+    
+    
+    
+
+   
